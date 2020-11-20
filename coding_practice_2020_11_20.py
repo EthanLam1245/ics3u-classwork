21316fe272ae579472c97cb01f11308cc1632bbc
EthanLam1245 <74618941+EthanLam1245@users.noreply.github.com>
2020-11-20 13:12:37 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -1,5 +1,12 @@
 # Coding Practice
 # November 20, 2020
 
+print("Problem 1")
 Colour = input("Please enter your favourite colour: ")
 print(f"{Colour}?! No way, that's my favourite colour too!")
+
+print("")
+print("Problem 2")
+NumberOfCansPerPack = int(input("Please enter the number of cans in the pack: "))
+NumberOfPacks = int(input("Please enter the number of packs: "))
+print(f"There are {NumberOfCansPerPack*NumberOfPacks} cans in total.")
