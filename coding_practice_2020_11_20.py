8447d0a30b04e44debf077c5ce7b28cdbf9a1207
EthanLam1245 <74618941+EthanLam1245@users.noreply.github.com>
2020-11-20 13:32:52 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -5,20 +5,20 @@ print("Problem 1")
 Colour = input("Please enter your favourite colour: ")
 print(f"{Colour}?! No way, that's my favourite colour too!")
 
-print("")
+print()
 print("Problem 2")
 NumberOfCansPerPack = int(input("Please enter the number of cans in the pack: "))
 NumberOfPacks = int(input("Please enter the number of packs: "))
 print(f"There are {NumberOfCansPerPack*NumberOfPacks} cans in total.")
 
-print("")
+print()
 print("Problem 3")
 FirstDimension = float(input("Please enter the first dimension of the rectangular prism: "))
 SecondDimension = float(input("Please enter the second dimension of the rectangular prism: "))
 ThirdDimension = float(input("Please enter the third dimension of the rectangular prism: "))
 print(f"The volume of this rectangular prism is {FirstDimension*SecondDimension*ThirdDimension} units cubed.")
 
-print("")
+print()
 print("Problem 4")
 Answer = input("Do you just join a Google Meet and mute the teacher? ")
 if Answer == "yes" or Answer == "Yes":
