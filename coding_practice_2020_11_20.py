13cee94cb961c86007a54008abcbbd150f336a2d
EthanLam1245 <74618941+EthanLam1245@users.noreply.github.com>
2020-11-20 13:41:37 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -2,31 +2,30 @@
 # November 20, 2020
 
 print("Problem 1")
-Colour = input("Please enter your favourite colour: ")
-print(f"{Colour}?! No way, that's my favourite colour too!")
+colour = input("Please enter your favourite colour: ")
+print(f"{colour}?! No way, that's my favourite colour too!")
 
 print()
 print("Problem 2")
-NumberOfCansPerPack = int(input("Please enter the number of cans in the pack: "))
-NumberOfPacks = int(input("Please enter the number of packs: "))
-NumberOfCans = NumberOfCansPerPack*NumberOfPacks
-print(f"There are {NumberOfCans} cans in total.")
+number_of_cans_per_pack = int(input("Please enter the number of cans in the pack: "))
+number_of_packs = int(input("Please enter the number of packs: "))
+number_of_cans = number_of_cans_per_pack * number_of_packs
+print(f"There are {number_of_cans} cans in total.")
 
 print()
 print("Problem 3")
-FirstDimension = float(input("Please enter the first dimension of the rectangular prism: "))
-SecondDimension = float(input("Please enter the second dimension of the rectangular prism: "))
-ThirdDimension = float(input("Please enter the third dimension of the rectangular prism: "))
-TotalVolume = FirstDimension*SecondDimension*ThirdDimension
-print(f"The volume of this rectangular prism is {TotalVolume} units cubed.")
+first_dimension = float(input("Please enter the first dimension of the rectangular prism: "))
+second_dimension = float(input("Please enter the second dimension of the rectangular prism: "))
+third_dimension = float(input("Please enter the third dimension of the rectangular prism: "))
+total_volume = first_dimension * second_dimension * third_dimension
+print(f"The volume of this rectangular prism is {total_volume} units cubed.")
 
 print()
 print("Problem 4")
-Answer = input("Do you just join a Google Meet and mute the teacher? ")
-if Answer == "yes" or Answer == "Yes":
+answer = input("Do you just join a Google Meet and mute the teacher? ")
+if answer == "yes" or answer == "Yes":
     print("That's probably not a good idea")
-elif Answer == "no" or Answer == "No":
+elif answer == "no" or answer == "No":
     print("Ok. Good.")
 else:
     print("That is not a valid answer!")
-
