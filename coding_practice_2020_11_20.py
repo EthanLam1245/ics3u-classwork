177175f36a0356b146f8339a957913a1d58e2909
EthanLam1245 <74618941+EthanLam1245@users.noreply.github.com>
2020-11-20 13:35:13 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -9,14 +9,16 @@ print()
 print("Problem 2")
 NumberOfCansPerPack = int(input("Please enter the number of cans in the pack: "))
 NumberOfPacks = int(input("Please enter the number of packs: "))
-print(f"There are {NumberOfCansPerPack*NumberOfPacks} cans in total.")
+NumberOfCans = NumberOfCansPerPack*NumberOfPacks
+print(f"There are {NumberOfCans} cans in total.")
 
 print()
 print("Problem 3")
 FirstDimension = float(input("Please enter the first dimension of the rectangular prism: "))
 SecondDimension = float(input("Please enter the second dimension of the rectangular prism: "))
 ThirdDimension = float(input("Please enter the third dimension of the rectangular prism: "))
-print(f"The volume of this rectangular prism is {FirstDimension*SecondDimension*ThirdDimension} units cubed.")
+TotalVolume = FirstDimension*SecondDimension*ThirdDimension
+print(f"The volume of this rectangular prism is {TotalVolume} units cubed.")
 
 print()
 print("Problem 4")
@@ -27,3 +29,4 @@ elif Answer == "no" or Answer == "No":
     print("Ok. Good.")
 else:
     print("That is not a valid answer!")
+
