1b6eb208d1a244dff26b055be9d51092c60a9c84
EthanLam1245 <74618941+EthanLam1245@users.noreply.github.com>
2020-11-27 13:40:05 -0500

    Create counter_variable_practice.py

---
+++ counter_variable_practice.py
@@ -0,0 +1,70 @@
+# Loop with a counter variable
+
+# 1. from 9 to 457 by 16.
+i = 9
+while i < 458:
+    print(i)
+    i += 16
+
+# 2. from -138 to 454 by 16.
+print()
+i = -138
+while i < 455:
+    print(i)
+    i += 16
+
+# 3. from -238 to -84 by 11.
+print()
+i = -238
+while i <= -84:
+    print(i)
+    i += 11
+
+# 4. from -52 to 438 by 10.
+print()
+i = -52
+while i <= 438:
+    print(i)
+    i += 10
+
+# 5. from 138 to 158 by 2.
+print()
+i = 138
+while i <= 158:
+    print(i)
+    i += 2
+
+# 6. from 1 to 144 by 11.
+print()
+i = 1
+while i <= 144:
+    print(i)
+    i += 11
+# 7. from 277 to 277 by 0.
+print()
+i = 277
+while i <= 277:
+    print(i)
+    i += 0
+    break # This creates an infinite loop so it must be broken
+
+# 8. from -384 to -306 by 2.
+print()
+i = -384
+while i <= -306:
+    print(i)
+    i += 2
+
+# 9. from 283 to 295 by 1.
+print()
+i = 283
+while i <= 295:
+    print(i)
+    i += 1
+
+# 10. from -2 to 52 by 3.
+print()
+i = -2
+while i <= 52:
+    print(i)
+    i += 3
