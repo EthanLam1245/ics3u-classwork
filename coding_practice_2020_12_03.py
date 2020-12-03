0a6f68af6dd1521aa1fc5674cdd50bee29ab474f
EthanLam1245 <74618941+EthanLam1245@users.noreply.github.com>
2020-12-03 15:17:34 -0500

    Update coding_practice_2020_12_03.py

--- coding_practice_2020_12_03.py
+++ coding_practice_2020_12_03.py
@@ -1,6 +1,16 @@
 # Function Practice
 # Question 1
 def add_3_numbers(a: float, b: float, c: float) -> float:
+    """Adds three numbers together.
+    
+    Args:
+        a: A number
+        b: Another number
+        c: The third number
+       
+    Returns:
+        The sum of the numbers.
+    """
     return a + b + c
 
 sum_of_numbers = add_3_numbers(5.6, 8.2, 12.13)
@@ -9,6 +19,15 @@ print()
 
 # Question 2
 def string_message(name: str, age: int) -> str:
+    """Takes a name and an age and returns a stirng message.
+    
+    Args:
+        name: User's name
+        age: User's age
+       
+    Returns:
+        A string message with both name and age.
+    """
     message = f"Hey {name}! I know you are {age} years old! How are you?"
     return message
 
@@ -18,6 +37,15 @@ print()
 
 # Question 3
 def average_of_numbers(num1: float, num2: float) -> float:
+    """Finds the average of two numbers.
+    
+    Args:
+        num1: A number
+        num2: Another number
+       
+    Returns:
+        The average of the numbers.
+    """
     average = (num1 + num2) / 2
     return average
 
@@ -27,6 +55,16 @@ print()
 
 # Question 4
 def maximum_number(n1: float, n2: float, n3: float) -> float:
+    """Finds the maximum value from three numbers.
+    
+    Args:
+        n1: First number
+        n2: Second number
+        n3: Third number
+       
+    Returns:
+        The largest number of the three.
+    """
     if n1 > n2 and n1 > n3:
         return n1
     elif n2 > n1 and n2 > n3:
@@ -42,6 +80,14 @@ print()
 
 # Question 5
 def first_two(string: str) -> str:
+    """Slices the first two characters in a given string.
+    
+    Args:
+        string: A string
+       
+    Returns:
+        The first two characters of the string.
+    """
     return string[0:2]
 
 new_string = first_two("Hello World")
