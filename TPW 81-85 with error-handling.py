96601b2b3f6f76f84fad38748c405d0e3f023215
EthanLam1245 <74618941+EthanLam1245@users.noreply.github.com>
2021-01-05 10:06:27 -0500

    Update TPW 81-85 with error-handling.py

--- TPW 81-85 with error-handling.py
+++ TPW 81-85 with error-handling.py
@@ -86,17 +86,42 @@ def main():
 
 #Exercise 81: Compute the Hypotenuse
 def calculate_hypotenuse(first_side: float, second_side: float) -> float:
+    """Calculate the hypotenuse of a right angle triangle.
+
+    Args:
+        first_side: The shortest side of the triangle.
+        second_side: The other shortest side of the triangle.
+    
+    Returns:
+        The length of the hypotenuse.
+    """
     return math.sqrt(first_side ** 2 + second_side ** 2)
 
 
 #Exercise 82: Taxi Fare
 def total_fare(distance_traveled: float) -> float:
+    """Calculate the taxi fare.
+
+    Args:
+        distance_traveled: The distance traveled in kilometers.
+
+    Returns:
+        The cost of the fare in dollars.
+    """
     multiplier = int(str(distance_traveled * 1000 / 140)[0])
     return base_fare + variable * multiplier
 
 
 #Exercise 83: Shipping Calculator
 def shipping_charge(number_of_items: int) -> float:
+    """Calculate the shipping cost.
+
+    Args: 
+        number_of_items: The number of items needed for shipping.
+
+    Returns:
+        The cost for shipping the items in dollars.
+    """
     if number_of_items > 0:
         return first_item + subsequent_items * (number_of_items - 1)
     else:
@@ -105,6 +130,16 @@ def shipping_charge(number_of_items: int) -> float:
 
 #Exercise 84: Median of Three Values
 def calculate_median(num1: float, num2: float, num3: float) -> float:
+    """Calculate the median of 3 numbers.
+
+    Args:
+        num1: First number
+        num2: Second number
+        num3: Third number
+    
+    Returns:
+        The median of the 3 numbers.
+    """
     unsorted = [num1, num2, num3]
     largest = max(unsorted)
     smallest = min(unsorted)
@@ -115,6 +150,14 @@ def calculate_median(num1: float, num2: float, num3: float) -> float:
 
 #Exercise 85: Convert an Integer to its Ordinal Number
 def ordinal_number(integer: int) -> str:
+    """Identifies the ordinal number from 1 through 12.
+
+    Args:
+        integer: An integer between 1 and 12 (inclusive)
+    
+    Returns:
+        The ordinal number of that integer.
+    """
     english_ordinal_numbers = "first second third fourth fifth sixth seventh eight ninth tenth eleventh twelfth".split(" ")
     if 1 <= integer <= 12:
         return english_ordinal_numbers[integer - 1]
